911c498566b675772849050e5a59f0345793406a
Rebekah Veiga <61145169+rebekahveiga@users.noreply.github.com>
2023-01-30 00:11:11 -0300

    Add files via upload

---
+++ components/ModalOrder/index.tsx
@@ -0,0 +1,47 @@
+import Modal from 'react-modal'
+import styles from './styles.module.scss'
+import { FiX } from 'react-icons/fi'
+import { OrderItemProps } from '../../pages/dashboard'
+interface ModalOrderProps{
+    isOpen: boolean;
+    onRequestClose: () => void;
+    order: OrderItemProps[];
+
+}
+
+export function ModalOrder({isOpen, onRequestClose, order}: ModalOrderProps){
+    const customStyles = {
+        content:{
+            top: '50%',
+            bottom: 'auto',
+            left: '50%',
+            right: 'auto',
+            padding: '30px',
+            transform: 'translate(-50%, -50%)',
+            backgroundColor: '#1d1d2e'
+        }
+    };
+    
+    return(
+        <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        style={customStyles}
+        
+        >
+
+            <button
+            type='button'
+            onClick={onRequestClose}
+            className="react-modal-close"
+            style={{background: 'transparent', border:0}}
+            >
+
+                <FiX size={45} color="#f34748"/>
+
+            </button>
+
+
+        </Modal>
+    )
+}
